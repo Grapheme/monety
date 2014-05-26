13531341564c71f724a3ed78e78984621348ae0e
vkharseev <vladimir@realitygroup.ru>
2014-05-26 14:08:42 +0400

    Селекты для форм

--- app/views/user-cabinet/forms/catalog/register-lot-form.blade.php
+++ app/views/user-cabinet/forms/catalog/register-lot-form.blade.php
@@ -1,98 +1,92 @@
 {{ Form::open(array('url'=>slink::createAuthLink('register-lot/store'),'role'=>'form','class'=>'smart-form','id'=>'register-lot-form','method'=>'post')) }}
-	<div class="row margin-top-10">
-		<section class="col col-6">
-			<div class="well">
-				<header>Чтобы выставить лот заполните форму:</header>
-				<fieldset>
-					<ul class="nav nav-tabs bordered">
-						<li class="active">
-							<a data-toggle="tab" href="#data">Данные</a>
-						</li>
-					@if(Allow::valid_access('downloads'))
-						<li>
-							<a data-toggle="tab" href="#images">Изображения</a>
-						</li>
-					@endif
-					</ul>
-					<div class="tab-content padding-10" id="productTabContent">
-						<div id="data" class="tab-pane fade active in">
-							<section>
-								<label class="label">Название лота</label>
-								<label class="input"> <i class="icon-append fa fa-list-alt"></i>
-									{{ Form::text('name','') }}
-								</label>
-							</section>
-						@if(!empty($categories))
-							<section>
-								<label class="label">Категория:</label>
-								<label class="select">
-									<select class="category-change" name="category" autocomplete="off">
-									@foreach($categories as $category_title => $category)
-										<optgroup label="{{ $category_title }}">
-										@if(!empty($category['sub_categories']))
-											@foreach($category['sub_categories'] as $sub_category)
-											<option value="{{ $sub_category['id'] }}">{{ $sub_category['title'] }}</option>
-											@endforeach
-										@endif
-										</optgroup>
-									@endforeach
-									</select> <i></i>
-								</label>
-							</section>
-						@else
-							{{ Form::hidden('category',0) }}
-						@endif
-					@if(!empty($productsExtendedAttributes))
-						@foreach($productsExtendedAttributes as $attrName => $attributes)
-							<section>
-								<?php $productAttr = array();?>
-								<label class="label">{{ $attrName }}</label>
-								<label class="select">
-									@foreach($attributes as $attribute)
-										<?php $productAttr[$attribute->title] = $attribute->title;?>
-									@endforeach
-									{{ Form::select('attribute['.$attrName.']',$productAttr,NULL,array('autocomplete'=>'off')) }} <i></i>
-								</label>
-							</section>
+	<header>Чтобы выставить лот заполните форму:</header>
+	<fieldset>
+		<ul class="nav nav-tabs bordered">
+			<li class="active">
+				<a data-toggle="tab" href="#data">Данные</a>
+			</li>
+		@if(Allow::valid_access('downloads'))
+			<li>
+				<a data-toggle="tab" href="#images">Изображения</a>
+			</li>
+		@endif
+		</ul>
+		<div class="tab-content margin-top-important-10" id="productTabContent">
+			<div id="data" class="tab-pane fade active in">
+				<section>
+					<label class="label">Название лота</label>
+					<label class="input"> <i class="icon-append fa fa-list-alt"></i>
+						{{ Form::text('name','') }}
+					</label>
+				</section>
+			@if(!empty($categories))
+				<section>
+					<label class="label">Категория:</label>
+					<label class="select">
+						<select class="select2" name="category" autocomplete="off">
+						@foreach($categories as $category_title => $category)
+							<optgroup label="{{ $category_title }}">
+							@if(!empty($category['sub_categories']))
+								@foreach($category['sub_categories'] as $sub_category)
+								<option value="{{ $sub_category['id'] }}">{{ $sub_category['title'] }}</option>
+								@endforeach
+							@endif
+							</optgroup>
 						@endforeach
-					@endif
-							<section>
-								<label class="label">Содержание</label>
-								<label class="textarea">
-									{{ Form::textarea('content','',array('class'=>'redactor')) }}
-								</label>
-							</section>
-							<section>
-								<label class="label">Способ продажи</label>
-								<div class="inline-group">
-									<label class="radio">
-										<input type="radio" id="lot-in-shop" name="radio-inline"><i></i>Выставить товар в магазин
-									</label>
-									<div class="note">
-										Товар будет выставлен по фиксированной цене.
-									</div>
-									<label class="radio">
-										<input type="radio" id="lot-in-auction" name="radio-inline"><i></i>Выставить товар на аукцион
-									</label>
-									<div class="note">
-										Товар будет выставлен на торги. Покупателем станет участник, предложивший наивысшую цену.
-									</div>
-								</div>
-							</section>
+						</select>
+					</label>
+				</section>
+			@else
+				{{ Form::hidden('category',0) }}
+			@endif
+		@if(!empty($productsExtendedAttributes))
+			@foreach($productsExtendedAttributes as $attrName => $attributes)
+				<section>
+					<?php $productAttr = array();?>
+					<label class="label">{{ $attrName }}</label>
+					<label class="select">
+						@foreach($attributes as $attribute)
+							<?php $productAttr[$attribute->title] = $attribute->title;?>
+						@endforeach
+						{{ Form::select('attribute['.$attrName.']',$productAttr,NULL,array('autocomplete'=>'off','class'=>'select2')) }}
+					</label>
+				</section>
+			@endforeach
+		@endif
+				<section>
+					<label class="label">Содержание</label>
+					<label class="textarea">
+						{{ Form::textarea('content','',array('class'=>'redactor')) }}
+					</label>
+				</section>
+				<section>
+					<label class="label">Способ продажи</label>
+					<div class="inline-group">
+						<label class="radio">
+							<input type="radio" id="lot-in-shop" name="radio-inline"><i></i>Выставить товар в магазин
+						</label>
+						<div class="note">
+							Товар будет выставлен по фиксированной цене.
 						</div>
-					@if(Allow::valid_access('downloads'))
-						<div id="images" class="tab-pane fade">
-							<div action="{{ slink::createAuthLink('catalogs/products/upload-product-photo') }}" class="dropzone dz-clickable" id="ProductImageDropZone"></div>
+						<label class="radio">
+							<input type="radio" id="lot-in-auction" name="radio-inline"><i></i>Выставить товар на аукцион
+						</label>
+						<div class="note">
+							Товар будет выставлен на торги. Покупателем станет участник, предложивший наивысшую цену.
 						</div>
-					@endif
 					</div>
-				</fieldset>
-				<footer>
-					<button type="submit" autocomplete="off" class="btn btn-success no-margin regular-10 uppercase btn-form-submit">
-						<i class="fa fa-spinner fa-spin hidden"></i> <span class="btn-response-text">Далее</span>
-					</button>
-				</footer>
+				</section>
 			</div>
-		</section>
-	</div>
+		@if(Allow::valid_access('downloads'))
+			<div id="images" class="tab-pane fade">
+				<div action="{{ slink::createAuthLink('catalogs/products/upload-product-photo') }}" class="dropzone dz-clickable" id="ProductImageDropZone"></div>
+			</div>
+		@endif
+		</div>
+	</fieldset>
+	<footer>
+		<button type="submit" autocomplete="off" class="btn btn-success no-margin regular-10 uppercase btn-form-submit">
+			<i class="fa fa-spinner fa-spin hidden"></i> <span class="btn-response-text">Далее</span>
+		</button>
+	</footer>
 {{ Form::close() }}
--- app/views/user-cabinet/register-lot.blade.php
+++ app/views/user-cabinet/register-lot.blade.php
@@ -1,16 +1,22 @@
 @extends('templates.dashboard')
 @section('style')
 <link rel="stylesheet" href="{{slink::path('css/redactor.css')}}" />
+<link rel="stylesheet" href="{{slink::path('theme/css/smart-form.css')}}" />
+<link rel="stylesheet" href="{{slink::path('theme/css/select2.css')}}" />
 @stop
 @section('content')
 <div class="row">
-	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-		@include('user-cabinet.forms.catalog.register-lot-form')
+	 <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 main-cont">
+	 	<section class="form-example">
+			@include('user-cabinet.forms.catalog.register-lot-form')
+		</section>
 	</div>
 </div>
 @stop
 @section('scripts')
 	{{HTML::script('js/account/user.js')}}
+	<script src="{{slink::path('js/vendor/dropzone.min.js')}}"></script>
+	<script src="{{slink::path('js/vendor/select2.min.js')}}"></script>
 	<script type="text/javascript">
 		if(typeof pageSetUp === 'function'){pageSetUp();}
 		if(typeof runFormValidation === 'function'){
@@ -21,5 +27,4 @@
 	</script>
 	<script src="{{slink::path('js/vendor/redactor.min.js')}}"></script>
 	<script src="{{slink::path('js/system/redactor-config.js')}}"></script>
-	<script src="{{slink::path('js/vendor/dropzone.min.js')}}"></script>
 @stop
